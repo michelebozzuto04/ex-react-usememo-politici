3e720558e762fc9cb934834237219954af79235f
michelebozzuto04 <michele.bozzuto04@gmail.com>
2025-10-22 13:08:08 +0200

    Bonus Completed

--- src/App.jsx
+++ src/App.jsx
@@ -19,6 +19,7 @@ function App() {
 
   const [politicians, setPoliticians] = useState([]);
   const [search, setSearch] = useState('');
+  const [selectedPosition, setSelectedPosition] = useState('');
 
   const getPoliticians = async () => {
     const response = await fetch('http://localhost:3333/politicians');
@@ -26,25 +27,56 @@ function App() {
     setPoliticians(data);
   };
 
+  const positions = useMemo(() => {
+    const uniquePositions = [];
+    politicians.forEach(p => {
+      if (!uniquePositions.includes(p.position)) {
+        uniquePositions.push(p.position);
+      }
+    });
+    return uniquePositions;
+  }, [politicians]);
+
+  console.log(positions)
+
   const searchResults = useMemo(() => {
-    return politicians.filter((p) => p.name.toLowerCase().includes(search) || p.biography.toLowerCase().includes(search));
-  }, [search, politicians])
+    const filteredByPosition = politicians.filter((p) => p.position.toLowerCase().includes(selectedPosition))
+    return filteredByPosition.filter((p) =>
+      p.name.toLowerCase().includes(search) ||
+      p.biography.toLowerCase().includes(search)
+    );
+  }, [search, politicians, selectedPosition]);
 
   useEffect(() => {
     getPoliticians();
   }, []);
+
+  console.log(selectedPosition)
 
   return (
     <div className='container'>
       <div className='listHeader'>
         <h1>Politicians</h1>
 
-        <input
-          value={search}
-          onChange={(e) => setSearch(e.target.value)}
-          type='text'
-          placeholder='Search...'
-        />
+        <div className='searchContainer'>
+          <input
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+            type='text'
+            placeholder='Search...'
+          />
+          <select
+            onChange={(e) => setSelectedPosition(e.target.value)}
+            value={selectedPosition}
+          >
+            <option value="">--Select a position--</option>
+            {positions.map((position, i) => {
+              return (
+                <option key={i} value={position.toLowerCase()}>{position}</option>
+              )
+            })}
+          </select>
+        </div>
       </div>
       <ul className='list'>
         {searchResults.map((p) => {
